744509517e687228e82cff771d6da77da2ec885f
Eric Kascic <eric.kascic@stelligent.com>
2015-01-26 13:27:28 -0500

    check for balanced subnets

--- lib/resources/subnets_resource.rb
+++ lib/resources/subnets_resource.rb
@@ -19,6 +19,20 @@ module Serverspec
       def size
         @subnets.size
       end
+
+      def evenly_split_across_az?(num_azs)
+        raise 'must specify at least 1' if num_azs < 1
+        subnet_grouping_by_az = @subnets.group_by(&:availability_zone_name)
+        return false if number_of_sizes_in_sub_arrays(subnet_grouping_by_az) != 1
+        return false if subnet_grouping_by_az.size != num_azs
+        true
+      end
+
+      private
+
+      def number_of_sizes_in_sub_arrays(arr)
+        arr.map { |sub_arr| sub_arr.size }.uniq.size
+      end
     end
   end
 end
