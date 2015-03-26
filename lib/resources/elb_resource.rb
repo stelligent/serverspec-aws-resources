554637ab1c1120de35dae27fc200b211d6711702
Eric Kascic <eric.kascic@stelligent.com>
2015-03-26 11:38:57 -0400

    add debug

--- lib/resources/elb_resource.rb
+++ lib/resources/elb_resource.rb
@@ -78,6 +78,8 @@ module Serverspec
       end
 
       def has_number_of_listeners?(number)
+        puts "SIZE: #{content.listeners.enum.size}"
+        content.listeners.enum.each { |x| puts x }
         content.listeners.enum.size == number
       end
 
