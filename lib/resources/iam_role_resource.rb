2c31638ba76c5f1bb4e0ec09d1a898dd68725274
Eric Kascic <eric.kascic@stelligent.com>
2015-03-26 16:57:10 -0400

    strip this down to something a bit simpler for now

--- lib/resources/iam_role_resource.rb
+++ lib/resources/iam_role_resource.rb
@@ -24,7 +24,9 @@ module Serverspec
       end
 
       def has_assume_role_policy_document?(expected_assume_role_policy_document)
-        content[:assume_role_policy_document] == expected_assume_role_policy_document
+        actual_policy_document = JSON.parse(content[:assume_role_policy_document])
+        actual_statements = actual_policy_document['Statements'].map { |statement| {effect: statement['Effect'], principal: statement['Principal'], action: statement['Action']}}
+        Set.new(actual_statements) == expected_assume_role_policy_document
       end
 
       def has_policy_with_name?(expected_policy_name)
