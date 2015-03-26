877399683ca1afe29660ff54cd144d305a52be55
Eric Kascic <eric.kascic@stelligent.com>
2015-03-26 17:08:20 -0400

    debug.  really need to rework this with assertions that print out the comparison

--- lib/resources/iam_role_resource.rb
+++ lib/resources/iam_role_resource.rb
@@ -28,6 +28,8 @@ module Serverspec
         actual_policy_document = JSON.parse(URI.decode(content[:assume_role_policy_document]))
         puts actual_policy_document
         actual_statements = actual_policy_document['Statement'].map { |statement| {effect: statement['Effect'], principal: statement['Principal'], action: statement['Action']}}
+        puts actual_statements
+        puts expected_assume_role_policy_document
         Set.new(actual_statements) == expected_assume_role_policy_document
       end
 
