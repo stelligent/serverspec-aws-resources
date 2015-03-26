39d107341622becdb5e5f3201e4e1b091838b16a
Eric Kascic <eric.kascic@stelligent.com>
2015-03-26 12:25:44 -0400

    wip

--- lib/resources/s3_bucket_resource.rb
+++ lib/resources/s3_bucket_resource.rb
@@ -23,6 +23,16 @@ module Serverspec
         content.website?
       end
 
+      def has_logging?(target_bucket_name, prefix)
+        response = AWS::S3::Client.new.get_bucket_logging(bucket_name: target_bucket_name)
+        puts response.data
+        false
+      end
+
+      # def has_notifications?(array_of_event_and_topic_pairs)
+      # no API i can find for this?
+      # end
+
       def to_s
         "s3 bucket: #{@bucket_name}"
       end
