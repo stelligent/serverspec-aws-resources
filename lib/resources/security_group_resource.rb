b900890628b86e3e562bdc18d0086f3540ce6a80
Eric Kascic <eric.kascic@stelligent.com>
2015-04-20 11:05:29 -0400

    add method to get by id

--- lib/resources/security_group_resource.rb
+++ lib/resources/security_group_resource.rb
@@ -9,18 +9,32 @@ module Serverspec
 
       include Serverspec::Type::SecurityGroups
 
-      def initialize(sg_tag_name_value)
+      def initialize(sg_tag_name_value, sg_id=nil)
         @sg_tag_name_value = sg_tag_name_value
+        @sg_id = sg_id
       end
 
       def content
-        found_group_name = nil
+        if @sg_id.nil?
+          find_sg_by_name_tag
+        else
+          AWS::EC2.new.security_groups[@sg_id]
+        end
+      end
+
+      def to_s
+        @sg_tag_name_value
+      end
+
+      private
+
+      def find_sg_by_name_tag
+        found_group_id = nil
 
         AWS::EC2.new.security_groups.each do |group|
           group.tags.to_h.each do |tag_name, tag_value|
-            puts "TAG: #{tag_name} -> #{tag_value}"
             if tag_name == 'Name' and tag_value == @sg_tag_name_value
-              found_group_name = group.name
+              found_group_id = group.id
             end
           end
         end
@@ -28,18 +42,18 @@ module Serverspec
         if found_group_name == nil
           raise "no match found for #{@sg_tag_name_value}"
         else
-          AWS::EC2.new.security_groups[found_group_name]
+          AWS::EC2.new.security_groups[found_group_id]
         end
-      end
-
-      def to_s
-        @sg_tag_name_value
       end
     end
 
     #this is how the resource is called out in a spec
     def security_group(sg_tag_name_value)
       SecurityGroup.new(sg_tag_name_value)
+    end
+
+    def security_group_by_id(sg_id)
+      SecurityGroup.new(nil, sg_id)
     end
   end
 end
